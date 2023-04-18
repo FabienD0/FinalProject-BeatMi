d6d6ec75469924c46cc6be9021a4613f9f9cd995
FabienD0 <fabien.denommee@gmail.com>
2023-04-18 18:03:46 -0400

    pianoFeatureAdded

--- frontend/src/pages/Piano.js
+++ frontend/src/pages/Piano.js
@@ -1,148 +1,177 @@
 import styled from "styled-components";
 import Colors from "../utils/Colors";
+import { KEY_TO_NOTE, VALID_KEYS, pianoKey } from "../utils/data";
+import { useState } from "react";
+import { useContext } from "react";
+import { PlayerContext } from "../components/context/PlayerContext";
+import { useEffect } from "react";
 
 const Piano = () => {
-  const handleMouseUp = (e) => {
+  const [screenDisplay, setScreenDisplay] = useState("C#");
+  const [volume, setVolume] = useState(0);
+  const { playerChords } = useContext(PlayerContext);
+
+  const handleMouseUp = (e, typeKey) => {
     e.currentTarget.classList.remove("active");
   };
 
-  const handleMouseDown = (e) => {
+  /* Function when pressing a piano key */
+  const handleMouseDown = (e, typeKey, index) => {
     e.stopPropagation();
     e.currentTarget.classList.add("active");
+
+    let note = "";
+
+    if (typeKey === "black") {
+      note = pianoKey[parseInt(e.target.id)].key + "#";
+    } else {
+      note = e.target.id;
+    }
+
+    setScreenDisplay(note.replace(/[45]/g, ""));
+    playerChords.triggerAttackRelease(note, 2.5);
   };
+
+  /* Change Volume */
+  useEffect(() => {
+    if (playerChords) {
+      playerChords.volume.value = volume;
+    }
+  }, [volume]);
+
+  /* Handle Key Down to play the piano */
+  const handleKeyDown = (e) => {
+    if (e.repeat) return;
+    if (VALID_KEYS.includes(e.key)) {
+      const [filteredNote] = KEY_TO_NOTE.filter(
+        (note) => e.key === Object.keys(note)[0]
+      );
+      const note = filteredNote[e.key];
+
+      if (filteredNote.key === "black") {
+        const [blackKey] = document.getElementsByClassName(
+          note.replace("#", "")
+        );
+        blackKey.classList.add("active");
+      } else {
+        document.getElementById(note).classList.add("active");
+      }
+
+      setScreenDisplay(note.replace(/[45]/g, ""));
+      playerChords.triggerAttackRelease(note, 2.5);
+    }
+  };
+
+  /* Handle Key Up to remove the class */
+  const handleKeyUp = (e) => {
+    if (e.repeat) return;
+    if (VALID_KEYS.includes(e.key)) {
+      const [filteredNote] = KEY_TO_NOTE.filter(
+        (note) => e.key === Object.keys(note)[0]
+      );
+
+      const note = filteredNote[e.key];
+
+      if (filteredNote.key === "black") {
+        const [blackKey] = document.getElementsByClassName(
+          note.replace("#", "")
+        );
+
+        blackKey.classList.remove("active");
+      } else {
+        document.getElementById(note).classList.remove("active");
+      }
+    }
+  };
+
+  /* Add the Event Listener to click the key on keyboard */
+  useEffect(() => {
+    if (playerChords) {
+      document.addEventListener("keydown", handleKeyDown);
+      document.addEventListener("keyup", handleKeyUp);
+
+      return () => {
+        document.removeEventListener("keydown", handleKeyDown);
+        document.removeEventListener("keyup", handleKeyUp);
+      };
+    }
+  }, [playerChords]);
 
   return (
     <Container>
-      <ContainerTop>
+      <ContainerTitle>
         <Title>Piano</Title>
-      </ContainerTop>
+      </ContainerTitle>
       <ContainerBottom>
         <ContainerPiano>
-          <WhiteKey
-            onMouseUp={(e) => handleMouseUp(e)}
-            onMouseDown={(e) => handleMouseDown(e)}
-            backgroundColor={"#f399b0"}
-          >
-            <BlackKey
+          <ContainerTop>
+            <ContainerScreen>{screenDisplay}</ContainerScreen>
+            <ContainerSlider>
+              <ContainerVolumeBar>
+                <VolumeBar
+                  color={volume > -5 ? "#01d205" : "transparent"}
+                  border={volume > -5 ? "transparent" : "1px solid #01d205"}
+                  height={"40%"}
+                />
+                <VolumeBar
+                  color={volume > -3 ? "#6adc12" : "transparent"}
+                  border={volume > -3 ? "transparent" : "1px solid #6adc12"}
+                  height={"50%"}
+                />
+                <VolumeBar
+                  color={volume > -1 ? "#87e40d" : "transparent"}
+                  border={volume > -1 ? "transparent" : "1px solid #87e40d"}
+                  height={"60%"}
+                />
+                <VolumeBar
+                  color={volume >= 0 ? "#e4f84b" : "transparent"}
+                  border={volume >= 0 ? "transparent" : "1px solid #e4f84b"}
+                  height={"70%"}
+                />
+                <VolumeBar
+                  color={volume > 2 ? "#fbe750" : "transparent"}
+                  border={volume > 2 ? "transparent" : "1px solid #fbe750"}
+                  height={"80%"}
+                />
+                <VolumeBar
+                  color={volume > 3 ? "#fdac5b" : "transparent"}
+                  border={volume > 3 ? "transparent" : "1px solid #fdac5b"}
+                  height={"90%"}
+                />
+                <VolumeBar
+                  color={volume >= 5 ? "#ff615e" : "transparent"}
+                  border={volume >= 5 ? "transparent" : "1px solid #ff615e"}
+                  height={"100%"}
+                />
+              </ContainerVolumeBar>
+              <Slider
+                type="range"
+                min="-5"
+                max="5"
+                step={1}
+                value={volume}
+                onChange={(e) => setVolume(e.target.value)}
+              />
+            </ContainerSlider>
+          </ContainerTop>
+          {pianoKey.map((key, index) => (
+            <WhiteKey
+              key={index}
+              id={key.key}
               onMouseUp={(e) => handleMouseUp(e)}
-              onMouseDown={(e) => handleMouseDown(e)}
-            ></BlackKey>
-          </WhiteKey>
-          <WhiteKey
-            onMouseUp={(e) => handleMouseUp(e)}
-            onMouseDown={(e) => handleMouseDown(e)}
-            backgroundColor={"#15c6fc"}
-          >
-            <BlackKey
-              onMouseUp={(e) => handleMouseUp(e)}
-              onMouseDown={(e) => handleMouseDown(e)}
-            ></BlackKey>
-          </WhiteKey>
-          <WhiteKey
-            onMouseUp={(e) => handleMouseUp(e)}
-            onMouseDown={(e) => handleMouseDown(e)}
-            backgroundColor={"#fdd001"}
-          ></WhiteKey>
-          <WhiteKey
-            onMouseUp={(e) => handleMouseUp(e)}
-            onMouseDown={(e) => handleMouseDown(e)}
-            backgroundColor={"#becb01"}
-          >
-            <BlackKey
-              onMouseUp={(e) => handleMouseUp(e)}
-              onMouseDown={(e) => handleMouseDown(e)}
-            ></BlackKey>
-          </WhiteKey>
-          <WhiteKey
-            onMouseUp={(e) => handleMouseUp(e)}
-            onMouseDown={(e) => handleMouseDown(e)}
-            backgroundColor={"#0055d6"}
-          >
-            <BlackKey
-              onMouseUp={(e) => handleMouseUp(e)}
-              onMouseDown={(e) => handleMouseDown(e)}
-            ></BlackKey>
-          </WhiteKey>
-          <WhiteKey
-            onMouseUp={(e) => handleMouseUp(e)}
-            onMouseDown={(e) => handleMouseDown(e)}
-            backgroundColor={"#c30302"}
-          >
-            <BlackKey
-              onMouseUp={(e) => handleMouseUp(e)}
-              onMouseDown={(e) => handleMouseDown(e)}
-            ></BlackKey>
-          </WhiteKey>
-          <WhiteKey
-            onMouseUp={(e) => handleMouseUp(e)}
-            onMouseDown={(e) => handleMouseDown(e)}
-            backgroundColor={"#f19800"}
-          ></WhiteKey>
-          {/* <WhiteKey
-            onMouseUp={(e) => handleMouseUp(e)}
-            onMouseDown={(e) => handleMouseDown(e)}
-            backgroundColor={"#008980"}
-          ></WhiteKey> */}
-          <WhiteKey
-            onMouseUp={(e) => handleMouseUp(e)}
-            onMouseDown={(e) => handleMouseDown(e)}
-            backgroundColor={"#f399b0"}
-          >
-            <BlackKey
-              onMouseUp={(e) => handleMouseUp(e)}
-              onMouseDown={(e) => handleMouseDown(e)}
-            ></BlackKey>
-          </WhiteKey>
-          <WhiteKey
-            onMouseUp={(e) => handleMouseUp(e)}
-            onMouseDown={(e) => handleMouseDown(e)}
-            backgroundColor={"#15c6fc"}
-          >
-            <BlackKey
-              onMouseUp={(e) => handleMouseUp(e)}
-              onMouseDown={(e) => handleMouseDown(e)}
-            ></BlackKey>
-          </WhiteKey>
-          <WhiteKey
-            onMouseUp={(e) => handleMouseUp(e)}
-            onMouseDown={(e) => handleMouseDown(e)}
-            backgroundColor={"#fdd001"}
-          ></WhiteKey>
-          <WhiteKey
-            onMouseUp={(e) => handleMouseUp(e)}
-            onMouseDown={(e) => handleMouseDown(e)}
-            backgroundColor={"#becb01"}
-          >
-            <BlackKey
-              onMouseUp={(e) => handleMouseUp(e)}
-              onMouseDown={(e) => handleMouseDown(e)}
-            ></BlackKey>
-          </WhiteKey>
-          <WhiteKey
-            onMouseUp={(e) => handleMouseUp(e)}
-            onMouseDown={(e) => handleMouseDown(e)}
-            backgroundColor={"#0055d6"}
-          >
-            <BlackKey
-              onMouseUp={(e) => handleMouseUp(e)}
-              onMouseDown={(e) => handleMouseDown(e)}
-            ></BlackKey>
-          </WhiteKey>
-          <WhiteKey
-            onMouseUp={(e) => handleMouseUp(e)}
-            onMouseDown={(e) => handleMouseDown(e)}
-            backgroundColor={"#c30302"}
-          >
-            <BlackKey
-              onMouseUp={(e) => handleMouseUp(e)}
-              onMouseDown={(e) => handleMouseDown(e)}
-            ></BlackKey>
-          </WhiteKey>
-          <WhiteKey
-            onMouseUp={(e) => handleMouseUp(e)}
-            onMouseDown={(e) => handleMouseDown(e)}
-            backgroundColor={"#f19800"}
-          ></WhiteKey>
+              onMouseDown={(e) => handleMouseDown(e, "white", index)}
+              backgroundColor={key.color}
+            >
+              {index % 7 !== 2 && index % 7 !== 6 && (
+                <BlackKey
+                  id={index}
+                  className={key.key}
+                  onMouseUp={(e) => handleMouseUp(e, "black", index)}
+                  onMouseDown={(e) => handleMouseDown(e, "black", index)}
+                ></BlackKey>
+              )}
+            </WhiteKey>
+          ))}
         </ContainerPiano>
       </ContainerBottom>
     </Container>
@@ -158,7 +187,7 @@ const Container = styled.div`
   width: 100%;
 `;
 
-const ContainerTop = styled.div`
+const ContainerTitle = styled.div`
   text-align: center;
   width: 100%;
 `;
@@ -185,6 +214,7 @@ const Title = styled.p`
 `;
 
 const ContainerPiano = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -194,6 +224,7 @@ const ContainerPiano = styled.div`
   height: 35rem;
   width: 60rem;
   width: 95%;
+  font-family: "Electrolize", sans-serif;
 
   background: linear-gradient(
     45deg,
@@ -218,6 +249,99 @@ const ContainerPiano = styled.div`
 
   -webkit-box-shadow: 2px 3px 15px -6px rgba(238, 238, 238, 0.77);
   box-shadow: 2px 3px 15px -6px rgba(238, 238, 238, 0.77);
+`;
+
+const ContainerTop = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 10px;
+  gap: 5rem;
+  width: 100%;
+`;
+
+const ContainerScreen = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: black;
+  color: rgba(85, 255, 0, 0.7);
+  margin-left: 17%;
+  font-size: 3rem;
+  width: 15rem;
+  height: 5rem;
+  border: 2px solid rgba(255, 255, 255, 0.4);
+`;
+
+const ContainerSlider = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: 10%;
+  height: 5rem;
+`;
+
+const ContainerVolumeBar = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  height: 2.3rem;
+  width: 100%;
+  top: 0;
+`;
+
+const VolumeBar = styled.span`
+  background-color: ${(props) => props.color};
+  border: ${(props) => props.border};
+  height: ${(props) => props.height};
+  margin: 0 0.2rem;
+  width: calc(100% / 7);
+  border-radius: 2px;
+  transition: all 0.1s;
+`;
+
+const Slider = styled.input`
+  -webkit-appearance: none;
+  position: relative;
+  width: 100%;
+  height: 2px;
+  opacity: 0.7;
+  outline: none;
+  -webkit-transition: 0.2s;
+  transition: opacity 0.2s;
+
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 20px;
+    border-radius: 5px;
+    height: 40px;
+    height: 30px;
+    background: white;
+    cursor: pointer;
+  }
+
+  &::-moz-range-thumb {
+    width: 20px;
+    border-radius: 2px;
+    height: 20px;
+    background: #4caf50;
+    cursor: pointer;
+  }
+
+  &::after {
+    position: absolute;
+    content: "Volume";
+    color: ${Colors.gray};
+    font-weight: bold;
+    letter-spacing: 0.1rem;
+    font-size: 1rem;
+    bottom: -25px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
 `;
 
 const WhiteKey = styled.div`
--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -16,3 +16,78 @@ export const arrayNote = [
 export const arrayNoteNoNotation = ["C", "D", "E", "F", "G", "A", "B"];
 
 export const chordTonalityData = ["maj", "min", "dim", "sus4", "sus2", "aug"];
+
+//Piano Page
+export const pianoKey = [
+  { key: "C4", color: "#f399b0" },
+  { key: "D4", color: "#15c6fc" },
+  { key: "E4", color: "#fdd001" },
+  { key: "F4", color: "#becb01" },
+  { key: "G4", color: "#0055d6" },
+  { key: "A4", color: "#c30302" },
+  { key: "B4", color: "#f19800" },
+  { key: "C5", color: "#f399b0" },
+  { key: "D5", color: "#15c6fc" },
+  { key: "E5", color: "#fdd001" },
+  { key: "F5", color: "#becb01" },
+  { key: "G5", color: "#0055d6" },
+  { key: "A5", color: "#c30302" },
+  { key: "B5", color: "#f19800" },
+];
+
+export const KEY_TO_NOTE = [
+  //First Octave
+  { q: "C4" },
+  { 2: "C#4", key: "black", id: "0" },
+  { w: "D4" },
+  { 3: "D#4", key: "black", id: "1" },
+  { e: "E4" },
+  { r: "F4" },
+  { 5: "F#4", key: "black", id: "3" },
+  { t: "G4" },
+  { 6: "G#4", key: "black", id: "4" },
+  { y: "A4" },
+  { 7: "A#4", key: "black", id: "5" },
+  { u: "B4" },
+  //Second Octave
+  { i: "C5" },
+  { 9: "C#5", key: "black", id: "0" },
+  { o: "D5" },
+  { 0: "D#5", key: "black", id: "1" },
+  { p: "E5" },
+  { z: "F5" },
+  { s: "F#5", key: "black", id: "3" },
+  { x: "G5" },
+  { d: "G#5", key: "black", id: "4" },
+  { c: "A5" },
+  { f: "A#5", key: "black", id: "5" },
+  { v: "B5" },
+];
+
+export const VALID_KEYS = [
+  "q",
+  "2",
+  "w",
+  "3",
+  "e",
+  "r",
+  "5",
+  "t",
+  "6",
+  "y",
+  "7",
+  "u",
+  "i",
+  "9",
+  "o",
+  "0",
+  "p",
+  "z",
+  "s",
+  "x",
+  "d",
+  "c",
+  "f",
+  "v",
+];
+export const BLACK_KEYS = ["2", "3", "5", "6", "7"];
